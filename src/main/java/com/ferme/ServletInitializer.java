d742da7df7aea7abfcc1cfb8608965f3bc67c6d3
Victor Jaramillo <vjaramillo7@gmail.com>
2020-03-29 00:14:30 -0300

    Cambio de nombre en clase

--- src/main/java/com/ferme/ServletInitializer.java
+++ src/main/java/com/ferme/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(VaadinTest1Application.class);
+		return application.sources(FermeApplication.class);
 	}
 
 }
