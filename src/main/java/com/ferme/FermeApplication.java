4e787b56cb2e569f265585e1062f43074078bc20
Fabiola Orellana <fab.orellanag@alumnos.duoc.cl>
2020-04-12 15:13:35 -0400

    Implementación de Swagger

--- src/main/java/com/ferme/FermeApplication.java
+++ src/main/java/com/ferme/FermeApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@ComponentScan("com.ferme.*")
+@ComponentScan("com.ferme")
 public class FermeApplication {
 
 	public static void main(String[] args) {
